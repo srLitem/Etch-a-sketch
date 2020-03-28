1631258dc8d582bddd6870724c9a8131e17897ed
srLitem <santiagorhenals@gmail.com>
2020-03-28 17:46:49 +0100

    fix: Multiple boards are not allowed anymore

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
 // Obtaining the body from the document
 bodyDoc = document.querySelector('body');
+createButton = document.getElementById('creator');
+let block_list = [];
 
-//TODO: Make input fields for this
-let numCol = parseInt(prompt('Columns'));
-let numRow = parseInt(prompt('Rows'));
-createGrid(numCol, numRow);
+// Board creation on click
+createButton.addEventListener('click', (e) => {
+    // Clean board before adding a new one
+    if (document.getElementById('wrapper') != null) {
+        let deleteElem = document.getElementById('wrapper');
+        deleteElem.parentNode.removeChild(deleteElem);
+    }
+    let numCol = parseInt(prompt('Columns'));
+    let numRow = parseInt(prompt('Rows'));
+    createGrid(numCol, numRow);
 
-// Save the created blocks
-block_list = Array.from(document.getElementsByClassName('block'));
+    // Save the created blocks
+    block_list = Array.from(document.getElementsByClassName('block'));
 
-// Change the color of the hovered blocks
-block_list.forEach((block) => {
-    block.addEventListener('mouseover', (e) => {
-        block.style.backgroundColor = "blue";
+    // Change the color of the hovered blocks
+    block_list.forEach((block) => {
+        block.addEventListener('mouseover', (e) => {
+            block.style.backgroundColor = "blue";
+        })
     })
 })
 
+
+
 // Function created to generate the grid
-function createGrid(x, y){
+function createGrid(x, y) {
     // Setting the CSS variables
     document.documentElement.style.setProperty('--numCol', x);
     document.documentElement.style.setProperty('--numRow', y);
     let board = document.createElement('div')
-    board.classList.add('wrapper');
+    board.setAttribute('id', 'wrapper')
     bodyDoc.appendChild(board);
 
-    for(let i = 0; i < (x*y); i++){
+    for (let i = 0; i < (x * y); i++) {
         newDiv = document.createElement('div');
         newDiv.classList.add('block');
-        newDiv.setAttribute('id',(i+1));
+        newDiv.setAttribute('id', (i + 1));
         board.appendChild(newDiv);
     }
 }
