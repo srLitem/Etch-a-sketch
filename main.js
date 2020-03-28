43ff813f3fd4930e296c655dff4d3387d81a8275
srLitem <santiagorhenals@gmail.com>
2020-03-28 17:11:50 +0100

    feat: Change of color on hovering the blocks

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
+// Obtaining the body from the document
 bodyDoc = document.querySelector('body');
 
-function createGrid(x, y){
-    let board = document.createElement('div')
-    board.classList.add('wrapper');
-    bodyDoc.appendChild(board);
-    for(let i = 0; i < x; i++){
-        newColDiv = document.createElement('div');
-        newColDiv.classList.add('col');
-        newColDiv.setAttribute('id',(i+1));
-        board.appendChild(newColDiv);
-        for(let j = 0; j < y; j++){
-            newRowDiv = document.createElement('div');
-            newRowDiv.classList.add('row', 'square');
-            let idElement = `${i + 1}-${j + 1}`
-            newRowDiv.setAttribute('id', idElement);
-            newColDiv.appendChild(newRowDiv);
-        }
-    }
-
-}
-
+//TODO: Make input fields for this
 let numCol = parseInt(prompt('Columns'));
 let numRow = parseInt(prompt('Rows'));
 createGrid(numCol, numRow);
+
+// Save the created blocks
+block_list = Array.from(document.getElementsByClassName('block'));
+
+// Change the color of the hovered blocks
+block_list.forEach((block) => {
+    block.addEventListener('mouseover', (e) => {
+        block.style.backgroundColor = "blue";
+    })
+})
+
+// Function created to generate the grid
+function createGrid(x, y){
+    // Setting the CSS variables
+    document.documentElement.style.setProperty('--numCol', x);
+    document.documentElement.style.setProperty('--numRow', y);
+    let board = document.createElement('div')
+    board.classList.add('wrapper');
+    bodyDoc.appendChild(board);
+
+    for(let i = 0; i < (x*y); i++){
+        newDiv = document.createElement('div');
+        newDiv.classList.add('block');
+        newDiv.setAttribute('id',(i+1));
+        board.appendChild(newDiv);
+    }
+}
